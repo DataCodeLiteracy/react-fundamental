d2aca2e70db2272d7b78d274262231cc9b2aaeb2
DataCodeLiteracy <dataliteracy@icloud.com>
2023-10-12 15:43:42 +0900

    study: isSubmit으로 제출된 후에 input 값 초기화, name input에 focus
    * isSubmit
      - isSubmit으로 제출될때와 제출되기 전을 상태로 관리해서 제출되면 input의 값을 초기화 할
        수 있도록 구현
    
    * focus
      - useRef를 이용해서 input의 요소를 가지고 오고 그 요소의 current 값에 focus를 선언
      해서 submit이 이루어지고 나서 name input에 focus가 갈 수 있도록 구현
    
    2023.10.12

--- src/components/PaymentForm/PaymentForm.js
+++ src/components/PaymentForm/PaymentForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import './PaymentForm.css'
 
@@ -10,8 +10,13 @@ const PaymentForm = (props) => {
     date: new Date().toISOString().split('T')[0]
   })
 
+  const [isSubmit, setIsSubmit] = useState(true)
+
+  const nameRef = useRef(null)
+
   const inputHandler = (e) => {
     const { name, value } = e.target
+    setIsSubmit(true)
     setObjectState((prevState) => ({ ...prevState, [name]: value }))
   }
 
@@ -20,14 +25,16 @@ const PaymentForm = (props) => {
 
     props.getPaymentFormData(objectState)
 
-    console.log(typeof objectState.date)
-
     setObjectState({
       id: Math.random(),
       name: objectState.name,
       price: objectState.price,
       date: objectState.date
     })
+
+    setIsSubmit(false)
+
+    nameRef.current.focus()
   }
 
   return (
@@ -39,8 +46,9 @@ const PaymentForm = (props) => {
             <input
               type="text"
               onChange={inputHandler}
-              value={objectState.name}
+              value={isSubmit ? objectState.name : ''}
               name="name"
+              ref={nameRef}
             />
           </div>
           <div className="new-payment__control">
@@ -50,7 +58,7 @@ const PaymentForm = (props) => {
               min="0.01"
               step="0.01"
               onChange={inputHandler}
-              value={objectState.price}
+              value={isSubmit ? objectState.price : 0}
               name="price"
             />
           </div>
@@ -61,7 +69,11 @@ const PaymentForm = (props) => {
               min="2019-01-01"
               max="2023-12-31"
               onChange={inputHandler}
-              value={objectState.date}
+              value={
+                isSubmit
+                  ? objectState.date
+                  : new Date().toISOString().split('T')[0]
+              }
               name="date"
             />
           </div>
